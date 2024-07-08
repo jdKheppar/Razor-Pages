305676f1b063223cb6e242c519422a0678e4f039
Junaid <130550913+jdKheppar@users.noreply.github.com>
2024-07-08 17:33:52 +0500

    complete without functional db

--- Razor/Pages/Categories/Index.cshtml.cs
+++ Razor/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,23 @@
+//We have named the module Categories becuase toherwise we had to add the complete using statements
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor.Data;
+using Razor.Models;
 
 namespace Razor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        private readonly ApplicationDbContext _db;
+        private List<Category> CategoriList {  get; set; }
+        
+        //public IndexModel(ApplicationDbContext db)
+        //{
+        //    _db = db;
+        //}
+        //public void OnGet()
+        //{
+        //    CategoriList = _db.Categories.ToList();
+        //}
     }
 }
